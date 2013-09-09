be19d592ef4a157851027e9e08d22150952b247d
nivesh singh <nivesh2@gmail.com>
2013-09-09 11:18:46 -0400

    serverjs modified

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
-  app.use(app.router);
+  //app.use(app.router);
   app.use(express.static((path.join(__dirname, 'public'))));
 
 });
